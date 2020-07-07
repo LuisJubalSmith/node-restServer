eff6bf4d4353b6b447e0881d49d03f15491f29ad
luis <jubal8776@gmail.com>
2020-07-06 18:54:08 -0600

    Restserver - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,20 @@
 //Puerto
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+// Entorno
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// Base de datos
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cursoNode';
+} else {
+    urlDB = 'mongodb+srv://smith8776:Ls13246102@cluster0.hcptq.mongodb.net/cursoNode'
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,46 +1,30 @@
-require('./config/config')
+require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
-
 // parse application/x-www-form-urlencoded/use son middleware
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
 // parse application/json
 app.use(bodyParser.json());
+app.use(require('./routes/usuario'))
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario');
-});
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es nesesario',
-        });
-    } else {
-        res.json({
-            persona: body,
-        });
-    }
-});
+}, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
 
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id,
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario');
 });
 
 app.listen(process.env.PORT, () => {
