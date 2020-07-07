45bfec991131573c7fafdd3d7366dc6de24ae312
luis <jubal8776@gmail.com>
2020-07-06 22:51:40 -0600

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -14,7 +14,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cursoNode';
 } else {
-    urlDB = 'mongodb+srv://smith8776:Ls13246102@cluster0.hcptq.mongodb.net/cursoNode'
+    urlDB = process.env.NODE_ENV.MONGO_URL;
+    // 'mongodb+srv://smith8776:Ls13246102@cluster0.hcptq.mongodb.net/cursoNode'
 }
 
 process.env.URLDB = urlDB;
